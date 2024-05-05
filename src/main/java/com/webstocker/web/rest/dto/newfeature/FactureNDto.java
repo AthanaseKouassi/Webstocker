597b845101cde7971da4bb81b75741e93e531aa3
Athanase <athanasekouassi@hotmail.com>
2024-05-05 16:21:47 +0200

    suppression de lombok sur les DTO

--- src/main/java/com/webstocker/web/rest/dto/newfeature/FactureNDto.java
+++ src/main/java/com/webstocker/web/rest/dto/newfeature/FactureNDto.java
@@ -1,11 +1,9 @@
 package com.webstocker.web.rest.dto.newfeature;
 
 import com.webstocker.domain.enumeration.newfeature.StatutFacture;
-import lombok.Data;
 
 import java.time.LocalDate;
 
-@Data
 public class FactureNDto {
     private Long id;
     private LocalDate dateFacture;
@@ -19,4 +17,83 @@ public class FactureNDto {
     private Long idBonDeSortie;
 
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDateFacture() {
+        return dateFacture;
+    }
+
+    public void setDateFacture(LocalDate dateFacture) {
+        this.dateFacture = dateFacture;
+    }
+
+    public String getNomCommercial() {
+        return nomCommercial;
+    }
+
+    public void setNomCommercial(String nomCommercial) {
+        this.nomCommercial = nomCommercial;
+    }
+
+    public String getNomClient() {
+        return nomClient;
+    }
+
+    public void setNomClient(String nomClient) {
+        this.nomClient = nomClient;
+    }
+
+    public Long getMontantTotal() {
+        return montantTotal;
+    }
+
+    public void setMontantTotal(Long montantTotal) {
+        this.montantTotal = montantTotal;
+    }
+
+    public Long getResteApayer() {
+        return resteApayer;
+    }
+
+    public void setResteApayer(Long resteApayer) {
+        this.resteApayer = resteApayer;
+    }
+
+    public StatutFacture getStatutFacture() {
+        return statutFacture;
+    }
+
+    public void setStatutFacture(StatutFacture statutFacture) {
+        this.statutFacture = statutFacture;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public Long getIdClient() {
+        return idClient;
+    }
+
+    public void setIdClient(Long idClient) {
+        this.idClient = idClient;
+    }
+
+    public Long getIdBonDeSortie() {
+        return idBonDeSortie;
+    }
+
+    public void setIdBonDeSortie(Long idBonDeSortie) {
+        this.idBonDeSortie = idBonDeSortie;
+    }
 }
--- src/main/java/com/webstocker/web/rest/dto/newfeature/ReglementDto.java
+++ src/main/java/com/webstocker/web/rest/dto/newfeature/ReglementDto.java
@@ -1,14 +1,51 @@
 package com.webstocker.web.rest.dto.newfeature;
 
-import lombok.Data;
-
 import java.time.LocalDate;
 
-@Data
 public class ReglementDto {
     private Long id;
     private LocalDate dateReglement;
     private Long montantReglement;
     private Long idFacture;
     private Long idProduit;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDateReglement() {
+        return dateReglement;
+    }
+
+    public void setDateReglement(LocalDate dateReglement) {
+        this.dateReglement = dateReglement;
+    }
+
+    public Long getMontantReglement() {
+        return montantReglement;
+    }
+
+    public void setMontantReglement(Long montantReglement) {
+        this.montantReglement = montantReglement;
+    }
+
+    public Long getIdFacture() {
+        return idFacture;
+    }
+
+    public void setIdFacture(Long idFacture) {
+        this.idFacture = idFacture;
+    }
+
+    public Long getIdProduit() {
+        return idProduit;
+    }
+
+    public void setIdProduit(Long idProduit) {
+        this.idProduit = idProduit;
+    }
 }
